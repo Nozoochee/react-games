ec5e90cc5a15279c143cc728f799f3dbb3737380
Nozoochee <69016048+Nozoochee@users.noreply.github.com>
2022-12-20 15:46:32 +0100

    display cards for Memory

--- src/pages/Games/Memory/Memory.js
+++ src/pages/Games/Memory/Memory.js
@@ -1,5 +1,42 @@
+import './Memory.css';
+import { useState } from "react";
+import Card from "./Components/Card";
+
+
 export default function Memory(){
-    return <h1>Memory</h1>
+
+    const max_cards = 4;
+
+    //Init cards
+    //Chose random cards from them based on difficulty (unique)
+    //Duplicate the array elements
+    //Shuffle
+
+    let cardArray = [];
+    for (let i = 39; i <= 55; i++) {
+        cardArray.push({id: i, name: `test${i}`, status: '', img: `/images/${i}.png`})
+    }
+
+    let finalArr = [];
+    for (let i = 0; i < max_cards; i++) {
+        finalArr.push(cardArray[Math.floor(Math.random()*cardArray.length)])
+    }
+    for (let i = 0; i < max_cards; i++) {
+        finalArr.push(finalArr[i]);
+    }
+    finalArr.sort(() => Math.random() - 0.5)
+
+    const [cards, setCards] = useState(finalArr)
+
+    return (<>
+        <h1>Pokemon Memory</h1>
+        <div className="gamecontainer">
+            {cards.map((card, index) => {
+                return <Card card={card} key={index} />
+            })}
+        </div>
+    </>
+    )
 
     /*
     How to play:
